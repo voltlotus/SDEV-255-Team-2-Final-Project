1616f0edb7b4c7f9573ca860b8274d1171cf139b
Lauren <lntaylor.me@gmail.com>
2022-04-13 15:38:15 -0400

    Reverted changes to have operational bootstrap

--- app.js
+++ app.js
@@ -1,35 +1,35 @@
-const express = require('express');
+// const express = require('express');
 
-// express app
-const app = express();
+// // express app
+// const app = express();
 
-// listen for requests
-app.listen(3000);
+// // listen for requests
+// app.listen(3000);
 
-// register view engine
-app.set('view engine', 'ejs');
+// // register view engine
+// app.set('view engine', 'ejs');
 
-app.get('/', (req, res) => {
-    res.render('index', { title: 'Home' });
-});
+// app.get('/', (req, res) => {
+//     res.render('index', { title: 'Home' });
+// });
 
-app.get('/about', (req, res) => {
-    res.render('about', { title: 'About' });
-});
+// app.get('/about', (req, res) => {
+//     res.render('about', { title: 'About' });
+// });
 
-app.get('/course_list', (req, res) => {
-    res.render('course_list', { title: 'Course List' });
-});
+// app.get('/course_list', (req, res) => {
+//     res.render('course_list', { title: 'Course List' });
+// });
 
-app.get('/course_single', (req, res) => {
-    res.render('course_single', { title: 'Course Single' });
-});
+// app.get('/course_single', (req, res) => {
+//     res.render('course_single', { title: 'Course Single' });
+// });
 
-app.get('/login', (req, res) => {
-    res.render('login', { title: 'Log In' });
-});
+// app.get('/login', (req, res) => {
+//     res.render('login', { title: 'Log In' });
+// });
 
-// 404 page
-app.use((req, res) => {
-    res.status(404).render('404', { title: '404' });
-});
+// // 404 page
+// app.use((req, res) => {
+//     res.status(404).render('404', { title: '404' });
+// });
