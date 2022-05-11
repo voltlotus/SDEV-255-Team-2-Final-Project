d223e92a8d28c2682354e30677086db5a7945aa0
MStudent1 <mlancaster17@ivytech.edu>
2022-05-11 16:15:03 -0400

    Added code to connect to second database cluster for user data

---
+++ db.js
@@ -0,0 +1,7 @@
+// db.js cnnnects to user collection cluster on Mongodb Atlas
+const dbURI2 = "";
+const mongoose = require("mongoose");
+mongoose.set("useUnifiedTopology", true);
+mongoose.connect(dbURI2,
+{ useNewUrlParser: true });
+module.exports = mongoose;
