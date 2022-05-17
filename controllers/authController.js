213a95e7055e2c2db1fcc728add43ed8e5f99748
MStudent1 <mlancaster17@ivytech.edu>
2022-05-16 23:27:51 -0400

    Added error handler that should display multiple errors related to logging into the web application

--- controllers/authController.js
+++ controllers/authController.js
@@ -1,5 +1,20 @@
 const User = require('../models/user');
 
+//Error handler
+const handleErrors = (err) => {
+    console.log(err.message, err.code);
+    let error = { email: '', password: '' };
+
+    //Validation errors
+    if(err.message.includes('user validation failed')){
+        Object.values(err.error).forEach(({properties}) => {
+            error[properties.path] = properties.message;
+        });
+    }
+
+    return error;
+}
+
 module.exports.signup_get = (req, res) => {
     res.render('index');
 }
@@ -9,20 +24,20 @@ module.exports.login_get = (req, res) => {
 }
 
 module.exports.signup_post = async (req, res) => {
-    const { email, password} = req.body;
+    const { email, password } = req.body;
 
-    try{
+    try {
         const user = await User.create({ email, password });
         res.status(201).json(user);
     }
     catch (err) {
-        console.log(err);
+        const error = handleErrors(err);
         res.status(400).send('error, user not created');
     }
 }
 
 module.exports.login_post = async (req, res) => {
-    const { email, password} = req.body;
+    const { email, password } = req.body;
     console.log(email, password);
     res.send('user login');
 }
