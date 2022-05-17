4b56efa7c69a49fc971aef95b384a32fb8ca207c
MStudent1 <mlancaster17@ivytech.edu>
2022-05-16 22:31:24 -0400

    Not needed

--- db.js
+++
@@ -1,7 +0,0 @@
-// db.js connects to user collection cluster on Mongodb Atlas
-const dbURI2 = "";
-const mongoose = require("mongoose");
-mongoose.set("useUnifiedTopology", true);
-mongoose.connect(dbURI2,
-{ useNewUrlParser: true });
-module.exports = mongoose;
